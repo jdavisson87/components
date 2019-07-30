8e9665841e7a0f0f6de2f356d0ffb5a178d794db
Kittaka <jdavisson87@gmail.com>
2019-07-30 15:48:06 -0500

    Pomodoro 1
    Created Nav Component.  Created folders for components.  Using browser router for navigation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import Components from "./Components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/components" component={Components} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "@emotion/styled";
+
+export default function Nav() {
+  return (
+    <NavBar>
+      <LinkCtr>
+        <NavLink to="/">
+          <LinkText>Home</LinkText>
+        </NavLink>
+      </LinkCtr>
+      <LinkCtr>
+        <NavLink to="/components">
+          <LinkText>Components</LinkText>
+        </NavLink>
+      </LinkCtr>
+    </NavBar>
+  );
+}
+
+const NavBar = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+`;
+
+const LinkCtr = styled.div`
+  -o-transition: 0.7s;
+  -ms-transition: 0.7s;
+  -moz-transition: 0.7s;
+  -webkit-transition: 0.7s;
+  border-radius: 5px;
+  margin: 3px;
+  & > a {
+    text-decoration: none;
+    color: black;
+    :hover {
+      color: white;
+    }
+  }
+  :hover {
+    background: grey;
+    cursor: pointer;
+  }
+`;
+
+const LinkText = styled.p`
+  font-size: 25px;
+  font-weight: 100;
+  font-family: sans-serif;
+  -o-transition: 0.7s;
+  -ms-transition: 0.7s;
+  -moz-transition: 0.7s;
+  -webkit-transition: 0.7s;
+  margin: 0;
+  padding: 8px;
+`;
