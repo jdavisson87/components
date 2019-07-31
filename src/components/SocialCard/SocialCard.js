11760e3d62d4e60ca4546bb0841096734d1e33e5
Kittaka <jdavisson87@gmail.com>
2019-07-30 20:01:32 -0500

    Pomodoro 4
    Styled the Social Card

--- src/components/SocialCard/SocialCard.js
+++ src/components/SocialCard/SocialCard.js
@@ -30,15 +30,18 @@ export default function SocialCard() {
     from: {
       position: "absolute",
       transform: "rotateY(180deg)",
-      opacity: 0
+      opacity: 0,
+      transition: "all 0.5s linear"
     },
     enter: {
       transform: "rotateY(0deg)",
-      opacity: 1
+      opacity: 1,
+      transition: "all 0.5s linear"
     },
     leave: {
       transform: "rotateY(180deg)",
-      opacity: 0
+      opacity: 0,
+      transition: "all 0.5s linear"
     }
   });
 
@@ -49,19 +52,30 @@ export default function SocialCard() {
         item ? (
           <animated.div key={key} style={props}>
             <CardCtr onClick={() => setFlipped(!isFlipped)}>
-              <p>False</p>
+              <SocialMediaCtr>
+                <SocialMedia>LinkedIn Website:</SocialMedia>
+                <SocialMediaLink href={linkedIn}>{linkedIn}</SocialMediaLink>
+                <SocialMedia>GitHub Website:</SocialMedia>
+                <SocialMediaLink href={github}>{github}</SocialMediaLink>
+              </SocialMediaCtr>
+              <Footer>Click for Personal Network information</Footer>
             </CardCtr>
           </animated.div>
         ) : (
           <animated.div key={key} style={props}>
             <CardCtr onClick={() => setFlipped(!isFlipped)}>
+              <SocialCardHeader>
+                <Name>{name}</Name>
+              </SocialCardHeader>
               <InfoCtr>
                 <AvatarPic src={img} />
                 <PersonalInfo>
-                  <Name>{name}</Name>
                   <Position>{position}</Position>
+                  <Phone>{phone}</Phone>
+                  <Email href={`mailto:${email}`}>{email}</Email>
                 </PersonalInfo>
               </InfoCtr>
+              <Footer>Click for Social Network information</Footer>
             </CardCtr>
           </animated.div>
         )
@@ -77,6 +91,11 @@ const SocialCardCtr = styled.div`
   margin-top: 50px;
 `;
 
+const SocialCardHeader = styled.div`
+  background: blue;
+  width: 100%;
+`;
+
 const Title = styled.p`
   font-size: 24px;
   font-weight: bolder;
@@ -84,7 +103,9 @@ const Title = styled.p`
 
 const CardCtr = styled.div`
   display: flex;
+  flex-direction: column;
   width: 600px;
+  height: 200px;
   border: 1px solid black;
   border-radius: 5px;
   transform-style: preserve-3d;
@@ -101,8 +122,10 @@ const CardCtr = styled.div`
 
 const AvatarPic = styled.img`
   width: 100px;
+  height: 100px;
   border-radius: 50px;
   margin: 5px;
+  box-shadow: 5px 10px 8px #888888;
 `;
 
 const InfoCtr = styled.div`
@@ -110,9 +133,16 @@ const InfoCtr = styled.div`
   padding: 7px;
 `;
 
+const SocialMediaCtr = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 7px;
+`;
+
 const PersonalInfo = styled.div`
   display: flex;
   flex-direction: column;
+  padding-left: 15px;
   & > p {
     margin: 0;
   }
@@ -120,10 +150,38 @@ const PersonalInfo = styled.div`
 
 const Name = styled.p`
   font-weight: bolder;
-  font-size: 16px;
+  font-size: 20px;
+  margin: 10px;
 `;
 
 const Position = styled.p`
-  font-size: 10px;
+  font-size: 16px;
   font-weight: bold;
+`;
+
+const Phone = styled.p`
+  font-size: 16px;
+  font-weight: less;
+`;
+
+const Email = styled.a`
+  font-weight: bolder;
+`;
+
+const Footer = styled.p`
+  width: 100%;
+  margin: 0;
+  position: absolute;
+  bottom: 5px;
+  text-align: center;
+`;
+
+const SocialMedia = styled.p`
+  font-weight: bolder;
+  margin: 2px 3px;
+`;
+
+const SocialMediaLink = styled.a`
+  font-weight: less;
+  margin: 0px 5px;
 `;
