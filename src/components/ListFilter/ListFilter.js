d8222addc2105e62e3497842a3ed64504d30aaf1
Kittaka <jdavisson87@gmail.com>
2019-08-06 19:23:32 -0500

    Pomodoro 2
    Styled inputs and selectors for Component.

--- src/components/ListFilter/ListFilter.js
+++ src/components/ListFilter/ListFilter.js
@@ -6,9 +6,43 @@ export default function ListFilter() {
     <ListFilterCtr>
       <ComponentTitle>List Filter Component</ComponentTitle>
       <InputCtr>
-        <FilterInputs placeholder={"filter by"} />
-        <FilterInputs placeholder={"filter by"} />
-        <FilterInputs placeholder={"filter by"} />
+        <SelectorCtr>
+          <SelectorTitle>Filter By:</SelectorTitle>
+          <FilterInputs placeholder={"Name"} />
+        </SelectorCtr>
+        <SelectorCtr>
+          <SelectorTitle>Position</SelectorTitle>
+          <FilterSelector>
+            <option>All</option>
+            <option>QB</option>
+            <option>RB</option>
+            <option>FB</option>
+            <option>WR</option>
+            <option>TE</option>
+            <option>OL</option>
+            <option>DE</option>
+            <option>DT</option>
+            <option>LB</option>
+            <option>CB</option>
+            <option>S</option>
+            <option>P</option>
+            <option>K</option>
+            <option>LS</option>
+          </FilterSelector>
+        </SelectorCtr>
+        <SelectorCtr>
+          <SelectorTitle>Min/Max Age</SelectorTitle>
+          <MinMaxCtr>
+            <FilterSelector>
+              <option>Min</option>
+              <option>21</option>
+            </FilterSelector>
+            <FilterSelector>
+              <option>Max</option>
+              <option>37</option>
+            </FilterSelector>
+          </MinMaxCtr>
+        </SelectorCtr>
       </InputCtr>
       <HeaderCtr>
         <p>Column Names</p>
@@ -26,6 +60,7 @@ const ListFilterCtr = styled.div`
   align-items: center;
   text-align: center;
   width: 100%;
+  height: 150px;
 `;
 
 const ComponentTitle = styled.h2`
@@ -35,13 +70,15 @@ const ComponentTitle = styled.h2`
 const InputCtr = styled.div`
   display: flex;
   justify-content: flex-start;
+  align-items: center;
   width: 100%;
+  margin-top: 20px;
 `;
 
 const FilterInputs = styled.input`
   width: 200px;
   height: 24px;
-  margin: 20px 20px;
+  margin: 5px 20px;
   border: none;
   border-radius: 5px;
   font-weight: bolder;
@@ -57,8 +94,37 @@ const FilterInputs = styled.input`
   }
 `;
 
+const SelectorCtr = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  margin: 20px;
+`;
+
+const MinMaxCtr = styled.div`
+  display: flex;
+  width: 100%;
+  & > select {
+    width: 90px;
+  }
+`;
+
+const SelectorTitle = styled.p`
+  margin: 0;
+  font-size: 18px;
+  font-weight: bolder;
+`;
+
+const FilterSelector = styled.select`
+  height: 34px;
+  font-size: 18px;
+  margin: 5px;
+  background: lightgrey;
+`;
+
 const HeaderCtr = styled.div`
   background: red;
+  width: 100%;
 `;
 
 const ListCtr = styled.div`
